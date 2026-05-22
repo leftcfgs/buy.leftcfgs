dd05fcce681cd348e8d16f49cc779408bf3f54a0
leftcfgs <leftrivalscfgs@gmail.com>
2026-05-22 16:59:14 +0900

    Update Internal.lua

--- Internal.lua
+++ Internal.lua
@@ -6,7 +6,7 @@ local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
 
-print("=== NEXUS Rivals Skin Changer 複雑長め版 起動 ===")
+print("=== NEXUS Rivals Skin Changer ===")
 
 local SkinConfig = {
     Enabled = true,
